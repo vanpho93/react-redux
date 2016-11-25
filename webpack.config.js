131861ba9f645a3ee37df35e17996b61a03e7dd3
Admin <admin@Admins-MacBook-Pro.local>
2016-11-25 15:30:57 +0700

    boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var webpack = require('webpack');
+module.exports = {
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.js'
+  ],
+  externals:{
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  module:{
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query:{
+          presets: ['es2015', 'react', 'stage-0']
+        },
+        test: /\.jsx?$/,
+        exclude: /node_modules/
+      }
+    ]
+  }
+}
